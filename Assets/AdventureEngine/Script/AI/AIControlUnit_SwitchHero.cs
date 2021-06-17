e9827527a0503db8a2a81b60b4fbca2e8d27e9a7
zittacheng <zittacheng@gmail.com>
2021-06-17 06:57:37 -0400

    Tank

---
+++ Assets/AdventureEngine/Script/AI/AIControlUnit_SwitchHero.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ADV
+{
+    public class AIControlUnit_SwitchHero : AIControlUnit {
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
